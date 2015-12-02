3d08759fcd9fc414724c1baa5c5b655ccf8d80f9
jmatyja <januszmatyja@gmail.com>
2015-12-02 20:27:39 +0100

    add package

---
+++ FutConnector.js
@@ -0,0 +1,19 @@
+module.exports = function(config) {
+    var urls = {
+        'main'          : 'https://www.easports.com/uk/fifa/ultimate-team/web-app',
+        'login'         : 'https://www.easports.com/services/authenticate/login',
+        'nucleus'       : 'https://www.easports.com/iframe/fut16/?locale=en_GB&baseShowoffUrl=https%3A%2F%2Fwww.easports.com%2Fuk%2Ffifa%2Fultimate-team%2Fweb-app%2Fshow-off&guest_app_uri=http%3A%2F%2Fwww.easports.com%2Fuk%2Ffifa%2Fultimate-team%2Fweb-app',
+        'nucleus2'       : 'https://www.easports.com/iframe/fut16/?baseShowoffUrl=https%3A%2F%2Fwww.easports.com%2Fuk%2Ffifa%2Fultimate-team%2Fweb-app%2Fshow-off&guest_app_uri=http%3A%2F%2Fwww.easports.com%2Fuk%2Ffifa%2Fultimate-team%2Fweb-app&locale=en_GB',
+        'shards'        : 'http://www.easports.com/iframe/fut16/p/ut/shards?_=',
+        'accounts'      : 'https://www.easports.com/iframe/fut16/p/ut/game/fifa16/user/accountinfo?sku=FUT16WEB&_=1442505066982',
+        'sid'           : 'https://www.easports.com/iframe/fut16/p/ut/auth',
+        'validate'      : 'https://www.easports.com/iframe/fut16/p/ut/game/fifa16/phishing/validate',
+        'phishing'      : 'https://www.easports.com/iframe/fut16/p/ut/game/fifa16/phishing/question?_==',
+        'route'         : 'https://utas.s2.fut.ea.com:443',
+        'routeNoPort'   : 'https://utas.fut.ea.com'
+    }
+
+
+
+
+}
---
+++ FutRequest.js
@@ -0,0 +1,61 @@
+module.exports = function() {
+
+    function FutRequest(){
+        this.request = require('request');
+        this.merge = require('merge');
+        this.jar = this.request.jar();
+        this.optionsModified = {};
+        this.headersModified = {};
+        this.headersModifiedConstantly= {};
+
+        this.options = {
+            timeout: 30000,
+            method: 'GET',
+            jar: this.jar,
+            followAllRedirects :true
+
+        };
+
+        this.headers= {
+            'User-Agent': 'Mozilla/6.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Host': 'signin.ea.com',
+            'Connection': 'keep-alive',
+            'Origin': 'https://signin.ea.com'
+        };
+    }
+
+    FutRequest.prototype.ModifyOptions = function(options){
+        this.optionsModified = this.merge(this.options, options);
+    };
+    FutRequest.prototype.ModifyHeaders = function(headers){
+        this.headersModified = this.merge(this.headers, headers);
+    };
+    FutRequest.prototype.ModifyHeadersConstantly = function(headers){
+        this.headersModifiedConstantly = this.merge(this.headersModifiedConstantly, headers);
+    };
+    FutRequest.prototype.GetOptions = function(){
+        var options = this.optionsModified;
+        this.optionsModified = {};
+        options.headers = this.GetHeaders();
+        return options;
+
+    };
+    FutRequest.prototype.GetHeaders = function(){
+        var headers = this.merge(this.headers, this.merge(this.headersModifiedConstantly, this.headersModified));
+        this.headersModified = {};
+        return headers;
+    };
+    FutRequest.prototype.MakeRequest = function(successCallback, errorCallback){
+        this.request(this.GetOptions(), function(error, response, body){
+            if(error || !response || response.statusCode != '200'){
+                if(undefined != errorCallback && typeof(errorCallback) == 'function'){
+                    errorCallback(error, response);
+                }
+            } else {
+                successCallback(response, body);
+            }
+        });
+    };
+};
